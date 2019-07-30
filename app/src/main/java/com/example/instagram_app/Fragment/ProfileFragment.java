741e30bb79d74867cf1fcbf86d5ed897905b7e31
Meitar Cohen <meitarc@gmail.com>
2019-07-30 14:59:31 +0300

    add image viewer in profile

--- app/src/main/java/com/example/instagram_app/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/instagram_app/Fragment/ProfileFragment.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +20,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.instagram_app.Adapter.MyFotoAdapter;
 import com.example.instagram_app.Model.Post;
 import com.example.instagram_app.Model.User;
 import com.example.instagram_app.R;
@@ -28,10 +32,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class ProfileFragment extends Fragment {
     ImageView image_profile,options;
     TextView posts,following,followers,username,fullname,bio;
     Button edit_profile;
+
+    RecyclerView recyclerView;
+    MyFotoAdapter myFotoAdapter;
+    List<Post> postList;
 
     FirebaseUser firebaseUser;
     String profileid;
@@ -62,10 +75,19 @@ public class ProfileFragment extends Fragment {
         my_fotos = view.findViewById(R.id.my_fotos);
         saved_fotos = view.findViewById(R.id.saved_fotos);
 
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new GridLayoutManager(getContext(),3);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        postList = new ArrayList<>();
+        myFotoAdapter = new MyFotoAdapter(getContext(),postList);
+        recyclerView.setAdapter(myFotoAdapter);
+
+
         userInfo();
         getFollowers();
         getNrPosts();
-
+        myFotos();
         if(profileid.equals(firebaseUser.getUid()))
         {
             edit_profile.setText("Edit Profile");
@@ -199,4 +221,28 @@ public class ProfileFragment extends Fragment {
             }
         });
     }
+
+    private void myFotos(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                postList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    if(post.getPublisher().equals(profileid)){
+                        postList.add(post);
+                    }
+                }
+                Collections.reverse(postList);
+                myFotoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
