6e0e4636fed5e3d41df0ee64e0cb052fb39fee97
Meitar Cohen <meitarc@gmail.com>
2019-07-31 14:09:27 +0300

    fix save btn bug

--- app/src/main/java/com/example/instagram_app/EditProfileActivity.java
+++ app/src/main/java/com/example/instagram_app/EditProfileActivity.java
@@ -1,9 +1,5 @@
 package com.example.instagram_app;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -14,6 +10,10 @@ import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.instagram_app.Model.User;
@@ -28,8 +28,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
@@ -38,9 +40,9 @@ import java.util.HashMap;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    ImageView close,image_profile;
-    TextView save,tv_change;
-    MaterialEditText fullname,username,bio;
+    ImageView close, image_profile;
+    TextView save, tv_change;
+    MaterialEditText fullname, username, bio;
 
     FirebaseUser firebaseUser;
 
@@ -53,104 +55,104 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        close=findViewById(R.id.close);
-        image_profile=findViewById(R.id.image_profile);
-        save=findViewById(R.id.save);
-        tv_change=findViewById(R.id.tv_change);
-        fullname=findViewById(R.id.fullname);
-        username=findViewById(R.id.username);
-        bio=findViewById(R.id.bio);
+        close = findViewById(R.id.close);
+        image_profile = findViewById(R.id.image_profile);
+        save = findViewById(R.id.save);
+        tv_change = findViewById(R.id.tv_change);
+        fullname = findViewById(R.id.fullname);
+        username = findViewById(R.id.username);
+        bio = findViewById(R.id.bio);
 
-       firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageRef = FirebaseStorage.getInstance().getReference("uploads");
 
-        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("Users")
-                .child(firebaseUser.getUid());
-
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
         reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user =dataSnapshot.getValue(User.class);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
                 fullname.setText(user.getFullname());
                 username.setText(user.getUsername());
                 bio.setText(user.getBio());
                 Glide.with(getApplicationContext()).load(user.getImageurl()).into(image_profile);
-
-
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
+
         close.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 finish();
             }
         });
-        tv_change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CropImage.activity()
-                        .setAspectRatio(1,1)
-                        .setCropShape(CropImageView.CropShape.OVAL)
-                        .start(EditProfileActivity.this);
-            }
-        });
-        image_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CropImage.activity()
-                        .setAspectRatio(1,1)
-                        .setCropShape(CropImageView.CropShape.OVAL)
-                        .start(EditProfileActivity.this);
-            }
-        });
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 updateProfile(fullname.getText().toString(),
-                username.getText().toString(),
-                bio.getText().toString());
+                        username.getText().toString(),
+                        bio.getText().toString());
             }
         });
 
+        tv_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setAspectRatio(1,1)
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .start(EditProfileActivity.this);
+            }
+        });
 
+        image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setAspectRatio(1,1)
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .start(EditProfileActivity.this);
+            }
+        });
+    }
 
-    }
-    private void updateProfile(String username,String fullname,String bio)
-    {
-        DatabaseReference reference=FirebaseDatabase.getInstance().getReference("Users")
-                .child(firebaseUser.getUid());
-        HashMap<String,Object> hashMap=new HashMap<>();
-        hashMap.put("username",username);
-        hashMap.put("fullname",fullname);
-        hashMap.put("bio",bio);
+    private void updateProfile(String fullname, String username, String bio){
 
-        reference.updateChildren(hashMap);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.getUid());
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("fullname", fullname);
+        map.put("username", username);
+        map.put("bio", bio);
+
+        reference.updateChildren(map);
+
+        Toast.makeText(EditProfileActivity.this, "Successfully updated!", Toast.LENGTH_SHORT).show();
     }
-    private String getFileExtension(Uri uri)
-    {
-        ContentResolver contentResolver=getContentResolver();
-        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
-    private void uploadImage()
-    {
-        final ProgressDialog pd=new ProgressDialog(this);
+
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(this);
         pd.setMessage("Uploading");
         pd.show();
-        if(mImageUri!=null)
-        {
-            final StorageReference fileReference=storageRef.child(System.currentTimeMillis()
-                    +"."+getFileExtension(mImageUri));
-            uploadTask=fileReference.putFile(mImageUri);
-            uploadTask.continueWith(new Continuation() {
+        if (mImageUri != null){
+            final StorageReference fileReference = storageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            uploadTask = fileReference.putFile(mImageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                 @Override
-                public Object then(@NonNull Task task) throws Exception {
-                    if(task.isSuccessful())
-                    {
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
                         throw task.getException();
                     }
                     return fileReference.getDownloadUrl();
@@ -158,48 +160,46 @@ public class EditProfileActivity extends AppCompatActivity {
             }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                 @Override
                 public void onComplete(@NonNull Task<Uri> task) {
-                    if(task.isSuccessful()) {
-                        Uri downloadUri=task.getResult();
-                        String myUrl=downloadUri.toString();
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        String miUrlOk = downloadUri.toString();
 
-                        DatabaseReference reference=FirebaseDatabase.getInstance().getReference("Users")
-                                .child(firebaseUser.getUid());
-                        HashMap<String,Object>hashMap=new HashMap<>();
-                        hashMap.put("imageurl",""+myUrl);
-                        reference.updateChildren(hashMap);
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+                        HashMap<String, Object> map1 = new HashMap<>();
+                        map1.put("imageurl", ""+miUrlOk);
+                        reference.updateChildren(map1);
+
                         pd.dismiss();
-                    }
-                    else
-                    {
-                        Toast.makeText(EditProfileActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(EditProfileActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                     }
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(EditProfileActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EditProfileActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
-        }
-        else
-        {
-            Toast.makeText(this,"no image selected",Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(EditProfileActivity.this, "No image selected", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode==RESULT_OK)
-        {
-            CropImage.ActivityResult result=CropImage.getActivityResult(data);
-            mImageUri=result.getUri();
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            mImageUri = result.getUri();
 
             uploadImage();
-        }
-        else
-        {
-            Toast.makeText(this,"Something gone wrong!",Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "Something gone wrong!", Toast.LENGTH_SHORT).show();
         }
     }
 }
