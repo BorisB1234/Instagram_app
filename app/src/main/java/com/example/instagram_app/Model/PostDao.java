a7f54bf6074566709c30bfe01ae644642aa7f7b6
Meitar Cohen <meitarc@gmail.com>
2019-08-06 15:34:59 +0300

    add room

---
+++ app/src/main/java/com/example/instagram_app/Model/PostDao.java
@@ -0,0 +1,35 @@
+package com.example.instagram_app.Model;
+
+import android.os.AsyncTask;
+
+import androidx.core.util.Consumer;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PostDao {
+    @Query("select * from Post")
+    List<Post> getAll();
+
+    @Query("select * from Post")
+    LiveData<List<Post>> getAllPosts();
+
+    @Query("select * from Post where postid = :id")
+    Post get(String id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(Post... Posts);
+
+    @Insert
+    void insert(List<Post> Posts);
+
+    @Delete
+    void delete(Post Post);
+}
+
